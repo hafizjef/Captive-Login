e2aed50fdb9c5f57d42eff050b5af2c349efa857
Hafiz Jefri <hafiz.jefri@gmail.com>
2016-10-12 19:36:03 +0800

    Init

---
+++ Captived/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Captived
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.unmaskPass = new System.Windows.Forms.CheckBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(82, 12);
+            this.usernameBox.MaxLength = 10;
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(126, 20);
+            this.usernameBox.TabIndex = 0;
+            this.usernameBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.usernameBox.TextChanged += new System.EventHandler(this.usernameBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password : ";
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(82, 38);
+            this.passBox.MaxLength = 6;
+            this.passBox.Name = "passBox";
+            this.passBox.PasswordChar = '*';
+            this.passBox.Size = new System.Drawing.Size(126, 20);
+            this.passBox.TabIndex = 3;
+            this.passBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.passBox.TextChanged += new System.EventHandler(this.passBox_TextChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Enabled = false;
+            this.btnLogin.Location = new System.Drawing.Point(133, 98);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // unmaskPass
+            // 
+            this.unmaskPass.AutoSize = true;
+            this.unmaskPass.Location = new System.Drawing.Point(82, 64);
+            this.unmaskPass.Name = "unmaskPass";
+            this.unmaskPass.Size = new System.Drawing.Size(102, 17);
+            this.unmaskPass.TabIndex = 5;
+            this.unmaskPass.Text = "Show Password";
+            this.unmaskPass.UseVisualStyleBackColor = true;
+            this.unmaskPass.CheckedChanged += new System.EventHandler(this.unmaskPass_CheckedChanged);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "WiFi login are running in background to keep \r\nyour captive session alive";
+            this.notifyIcon.BalloonTipTitle = "Info";
+            this.notifyIcon.ContextMenuStrip = this.contextMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Captive";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showInfoToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(128, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // showInfoToolStripMenuItem
+            // 
+            this.showInfoToolStripMenuItem.Name = "showInfoToolStripMenuItem";
+            this.showInfoToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.showInfoToolStripMenuItem.Text = "Show Info";
+            this.showInfoToolStripMenuItem.Click += new System.EventHandler(this.showInfoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 133);
+            this.Controls.Add(this.unmaskPass);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usernameBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto WiFi Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeBegin += new System.EventHandler(this.Form1_Resize);
+            this.SizeChanged += new System.EventHandler(this.Form1_Resize);
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox unmaskPass;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showInfoToolStripMenuItem;
+    }
+}
+
---
+++ Captived/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Configuration;
+
+namespace Captived
+{
+    public partial class Form1 : Form
+    {
+        private string url = "http://172.16.202.1:8090/httpclient.html";
+        private string KPUrl = "http://172.16.202.1:8090/live";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void unmaskPass_CheckedChanged(object sender, EventArgs e)
+        {
+            passBox.PasswordChar = unmaskPass.Checked ? '\0' : '*';
+        }
+
+        private async void doLogin(string username, string password)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var values = new Dictionary<string, string>
+                    {
+                        { "mode", "191" },
+                        { "username", username },
+                        { "password", password }
+                     };
+
+                    var content = new FormUrlEncodedContent(values);
+                    var response = await client.PostAsync(url, content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    if (responseString.Contains("You have successfully logged in"))
+                    {
+                        // Login Success
+                        notifyIcon.BalloonTipText = "Login successfull";
+                        notifyIcon.ShowBalloonTip(500);
+                    }
+                    else
+                    {
+                        showUI();
+                        MessageBox.Show("Usename/Password combination error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    while (true)
+                    {
+                        await Task.Delay(180000);
+                        System.Net.WebClient webClient = new System.Net.WebClient();
+                        webClient.QueryString.Add("mode", "192");
+                        webClient.QueryString.Add("username", username);
+                        string result = webClient.DownloadString(KPUrl);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+
+        private void checkState()
+        {
+            if (usernameBox.Text == "" || passBox.Text == "")
+            {
+                btnLogin.Enabled = false;
+            }
+            else
+            {
+                btnLogin.Enabled = true;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            doLogin(usernameBox.Text, passBox.Text);
+            AddUpdateAppSettings("username", usernameBox.Text);
+            AddUpdateAppSettings("password", passBox.Text);
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void usernameBox_TextChanged(object sender, EventArgs e)
+        {
+            checkState();
+        }
+
+        private void passBox_TextChanged(object sender, EventArgs e)
+        {
+            checkState();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            usernameBox.Text = ReadSetting("username");
+            passBox.Text = ReadSetting("password");   
+        }
+
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+
+        static String ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? null;
+                return(result);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return null;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            showUI();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            minimiseToBackground();
+        }
+
+        private void minimiseToBackground()
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+                notifyIcon.BalloonTipText = "WiFi login are running in background to keep your captive session alive";
+                notifyIcon.Visible = true;
+                notifyIcon.ShowBalloonTip(500);
+                Hide();
+            }
+
+            else if (WindowState == FormWindowState.Normal)
+            {
+                notifyIcon.Visible = false;
+            }
+        }
+
+        private void showUI()
+        {
+            Show();
+            Visible = true;
+            ShowInTaskbar = true;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void showInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programmed By : HafizJef\n", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
